dc44dbdc4b4c9ab4fe47787390aae85b7a8fc9f1
Le'Mashon <Leonardlemashon@gmail.com>
2025-02-14 14:11:55 +0300

    Update models.py

--- banking/models.py
+++ banking/models.py
@@ -1,37 +1,48 @@
 from django.db import models
 from django.utils import timezone
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
 # Create your models here.
 class EmployeeManager(BaseUserManager):
-    def create_user(self, email, name, password=None, role=""):
+    def create_user(self, username, email, password=None, role=""):
         if not email:
             raise ValueError("Users must have an email")
-        user = self.model(email=self.normalize_email(email), name=name, role=role)
+        if not username:
+            raise ValueError("Users must have a username")
+        
+        user = self.model(
+            username=username,
+            email=self.normalize_email(email),
+            role=role
+        )
         user.set_password(password) # hash password
         user.save(using=self.db)
         return user
     
-    def create_superuser(self, email, name, password=None):
-        user = self.create_user(email, name, password)
-        user.is_admin = True
+    def create_superuser(self, username, email, password=None):
+        user = self.create_user(username, email, password)
+        user.is_superuser = True
+        user.is_staff = True
         user.save(using=self._db)
         return user
 
-class Employees(AbstractBaseUser):
-    name = models.CharField(max_length=50)
+class Employees(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(max_length=20, unique=True)
     email = models.EmailField(unique=True)
-    role = models.CharField(max_length=50)
+    role = models.CharField(max_length=20)
     created_at = models.DateTimeField(default=timezone.now)
     updated_at = models.DateTimeField(auto_now=True)
 
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
     objects = EmployeeManager()
 
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['name']
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
 
     def __str__(self):
-        return self.email
+        return self.username
 
 class Customers(models.Model):
     name = models.TextField()
